1d2dfd618a91c678ea14ada463a6f3f11be9223d
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-08-06 16:51:38 +0200

    Update inflow conditions

--- examples/rest_client.jl
+++ examples/rest_client.jl
@@ -1,12 +1,17 @@
 # input parameters of the init and the step functions of the REST API of the flight path planner/ optimizer
 
-struct InflowConditions
-    wind_speed::Float64 # in m/s at 6 m height
+Base.@kwdef struct InflowConditions
+    wind_speed::Float64     # in m/s at 6 m height
     wind_direction::Float64 # in degrees, 0 = North, 90 = East
-    alpha::Float64 = 0.08163 # exponent of the wind profile law
-    z0::Float64 = 0.0002 # surface roughness                       [m]
-    profile_law::Int64 = 3 # 1=EXP, 2=LOG, 3=EXPLOG
-    turbulence::Float64 = 0.0 # in [0, 1], 0 = no turbulence, 1 = full turbulence
+    profile_law::Int64      # 0=CONST, 1=EXP, 2=LOG, 3=EXPLOG, 4=CUSTOM_LOG, 5=CUSTOM_EXP, 6=CUSTOM_JET
+    # the custom profiles are fitted using the heights and speeds given in the heights and speeds fields
+    # CUSTOM_JET: u(z) = u_bg(z) + U_J * exp(-(z - z_c)^2 / (2*sigma^2))
+    # the following fields are optional; the defaults given below are used if omitted
+    alpha::Float64 = 0.08163            # exponent of the wind profile law
+    z0::Float64 = 0.0002                # surface roughness                                          [m]
+    turbulence::Float64 = 0.0           # in [0, 1], 0 = no turbulence, 1 = full turbulence
+    heights::Vector{Float64} = [6.0]    # heights at which the wind speed is given
+    speeds::Vector{Float64} = [wind_speed] # wind speeds at the given heights
 end
 
 struct WinchParams
--- examples/rest_client.py
+++ examples/rest_client.py
@@ -12,11 +12,15 @@ class WinchParams(TypedDict):
 class InflowConditions(TypedDict):
     wind_speed: float                   # in m/s at 6 m height
     wind_direction: float               # in degrees, 0 = North, 90 = East
-    profile_law: Literal[0, 1, 2, 3]    # 0=CONST, 1=EXP, 2=LOG, 3=EXPLOG
+    profile_law: Literal[0, 1, 2, 3, 4, 5, 6] # 0=CONST, 1=EXP, 2=LOG, 3=EXPLOG, 4=CUSTOM_LOG, 5=CUSTOM_EXP, 6=CUSTOM_JET
+    # the custom profiles are fitted using the heights and speeds given in the heights and speeds fields
+    # CUSTOM_JET: u(z) = u_bg(z) + U_J * exp(-(z - z_c)^2 / (2*sigma^2))
     # the following fields are optional; the defaults given in the comments are used if omitted
     alpha: NotRequired[float]           # exponent of the wind profile law, default: 0.08163
     z0: NotRequired[float]              # surface roughness [m], default: 0.0002
     turbulence: NotRequired[float]      # in [0, 1], 0 = no turbulence, 1 = full turbulence, default: 0.0
+    heights: NotRequired[list[float]]   # heights at which the wind speed is given, default: [6.0]
+    speeds: NotRequired[list[float]]    # wind speeds at the given heights, default: [wind_speed]
 
 
 # should be 1000 points
