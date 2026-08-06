7d443222f3adfe5c94e801642545ce74eff7a277
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-08-06 12:22:42 +0200

    Add rest_client.py and rest_client.jl

--- examples/rest_client.jl
+++ examples/rest_client.jl
@@ -1,5 +1,14 @@
 # input parameters of the init and the step functions of the REST API of the flight path planner/ optimizer
 
+struct InflowConditions
+    wind_speed::Float64 # in m/s at 6 m height
+    wind_direction::Float64 # in degrees, 0 = North, 90 = East
+    alpha::Float64 = 0.08163 # exponent of the wind profile law
+    z0::Float64 = 0.0002 # surface roughness                       [m]
+    profile_law::Int64 = 3 # 1=EXP, 2=LOG, 3=EXPLOG
+    turbulence::Float64 = 0.0 # in [0, 1], 0 = no turbulence, 1 = full turbulence
+end
+
 struct WinchParams
     mode::String # valid values: "reelout", "reelin"
     k_v::Float64 # v_set = k_v * sqrt(force)
@@ -20,6 +29,7 @@ struct InitParams
     max_time::Float64 # maximum time for the simulation
     length::Float64   # initial length of the tether
     winch_params::WinchParams
+    inflow_conditions::InflowConditions
     trajectory::Trajectory
 end
 
--- examples/rest_client.py
+++ examples/rest_client.py
@@ -1,6 +1,6 @@
 # input parameters of the init and the step functions of the REST API of the flight path planner/ optimizer
 
-from typing import Literal, TypedDict
+from typing import Literal, NotRequired, TypedDict
 
 
 class WinchParams(TypedDict):
@@ -8,6 +8,15 @@ class WinchParams(TypedDict):
     k_v: float      # v_set = k_v * sqrt(force)
     f_min: float    # minimum winch force
     f_max: float    # maximum winch force
+
+class InflowConditions(TypedDict):
+    wind_speed: float                   # in m/s at 6 m height
+    wind_direction: float               # in degrees, 0 = North, 90 = East
+    profile_law: Literal[0, 1, 2, 3]    # 0=CONST, 1=EXP, 2=LOG, 3=EXPLOG
+    # the following fields are optional; the defaults given in the comments are used if omitted
+    alpha: NotRequired[float]           # exponent of the wind profile law, default: 0.08163
+    z0: NotRequired[float]              # surface roughness [m], default: 0.0002
+    turbulence: NotRequired[float]      # in [0, 1], 0 = no turbulence, 1 = full turbulence, default: 0.0
 
 
 # should be 1000 points
@@ -22,6 +31,7 @@ class InitParams(TypedDict):
     max_time: float    # maximum time for the simulation
     length: float      # initial length of the tether
     winch_params: WinchParams
+    inflow_conditions: InflowConditions
     trajectory: Trajectory
 
 
